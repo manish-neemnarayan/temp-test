569bc9e3ae759ed3214806252a11a600fbdd3e63
manish-neemnarayan <manish9301kumar@gmail.com>
2023-04-17 18:35:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var marks=[];
+for(let i=1;i<=5;i++)
+{
+    marks[i]=prompt("Enter marks of Student "+i+" :")
+}
+
+for(let j=1;j<=5;j++)
+{
+    console.log(marks[j]+" ");  //displaying on console
+    document.write(marks[j]+" "); //displaying on browser
+}
+
+for(let k=1;k<=5;k++)
+{
+    
+}
